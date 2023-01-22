b291ebdf0f94913e4925ec1e1396503c72b36657
pavan-shadow2019 <103202829+pavan-shadow2019@users.noreply.github.com>
2023-01-22 22:08:14 +0530

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,45 @@
+Question:
+
+Matching Strings
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+Problem Statement
+You are given two strings S and T. Determine whether it is possible to make S and T equal by doing the following operation at most once:
+
+Choose two adjacent characters in S and swap them.
+
+Note that it is allowed to choose not to do the operation.
+Input
+The input line contains two strings in separate lines.
+
+Constraints
+Each of S and T is a string of length between 2 and 100 (inclusive) consisting of lowercase English letters.
+S and T have the same length.
+Output
+If it is possible to make S and T equal by doing the operation in Problem Statement at most once, print Yes; otherwise, print No.
+Example
+Sample Input 1
+abc
+acb
+
+Sample Output 1
+Yes
+
+Sample Input 2
+aabb
+bbaa
+
+Sample Output 2
+No
+
+Sample Input 3
+abcde
+abcde
+
+Sample Output 3
+Yes
+
+
+************************************************
+Code:
+
